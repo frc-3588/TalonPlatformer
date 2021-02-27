4858aba1f49900ba2d5e71f5c0df8f937ad33032
Johlesa <joh.orm000@rentonstudent.us>
2021-02-27 23:07:23 +0000

    johlesa added comments to playscreen

--- core/src/com/mygdx/game/PlayScreen.java
+++ core/src/com/mygdx/game/PlayScreen.java
@@ -1,5 +1,11 @@
 package com.mygdx.game;
 
 public class PlayScreen {
+    //set parameters for the character
+        //set their name and number lives = 3
+
+    //set parameters for the enemies
+        //set lives = 1 and location (x,y)
+    
     
 }
