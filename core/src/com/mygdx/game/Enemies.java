9d3a8b69a4237eb011d3d4a02dbc6cf7106f1488
Johlesa <joh.orm000@rentonstudent.us>
2021-02-27 22:43:41 +0000

    Johlesa made changes to Enemies; they now have lives + position

--- core/src/com/mygdx/game/Enemies.java
+++ core/src/com/mygdx/game/Enemies.java
@@ -1,5 +1,25 @@
 package com.mygdx.game;
 
 public class Enemies {
+    private int enemyLives;
+    private int positionX;
+    private int positionY;
+
+    public Enemies(int enemyLives, int positionX, int positionY){
+        this.enemyLives = enemyLives;
+        this.positionX = positionX;
+        this.positionY = positionY;
+    }
+
+    public int getEnemyLives(){
+        return enemyLives;
+    }
     
+    public int getPositionX(){
+        return positionX;
+    }
+
+    public int getPositionY(){
+        return positionY;
+    }
 }
