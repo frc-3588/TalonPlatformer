c83c5ad0d52020b1f4ea25d6e1ced3bd041a6669
Johlesa <joh.orm000@rentonstudent.us>
2021-02-27 22:24:33 +0000

    edited Player class

--- core/src/com/mygdx/game/Player.java
+++ core/src/com/mygdx/game/Player.java
@@ -1,5 +1,19 @@
 package com.mygdx.game;
 
 public class Player {
+    private int lives;
+    private String playerName;
+
+    public Player(String playerName, int lives){
+        this.playerName = playerName;
+        this.lives = lives;
+    }
+    public int getLives(){
+        return lives;
+    }
+    public String getName(){
+        return playerName;
+    }
+
     
 }
