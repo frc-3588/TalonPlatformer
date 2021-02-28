d1a8aa5ff9ea25b442d7dc22c305e00e99976ba8
Johlesa <joh.orm000@rentonstudent.us>
2021-02-28 21:02:08 +0000

    Johlesa modified PlayScreen class

--- core/src/com/mygdx/game/PlayScreen.java
+++ core/src/com/mygdx/game/PlayScreen.java
@@ -1,11 +1,40 @@
 package com.mygdx.game;
 
 public class PlayScreen {
-    //set parameters for the character
-        //set their name and number lives = 3
+    private boolean playerIsDead = false;
+    
+    public static void main(String[] args){
 
-    //set parameters for the enemies
-        //set lives = 1 and location (x,y)
+        //set parameters for the character (name, # lives)
+        Player myPlayer = new Player("Player 1", 3);
+
+        //set parameters for the enemies (lives, location(x,y))
+        int coordinateX = 0;
+        int coordinateY = 0;
+        Enemies myEnemy1 = new Enemies(1, coordinateX, coordinateY);
+        //this only creates 1 enemy
+        //repeat process, labeling myEnemy2, myEnemy3, etc. for more enemies
+
+        //initiate World gravity
+
+
+
+    }
+
+    //CHARACTER methods
+    public void livesDeductor(){
+        
+    }
+
+    //ENEMIES methods
+    public void enemyLivesDeductor(){
+
+    }
+    //GRAVITY/MOTIONS methods
+
+    //
+
+   
     
     
 }
