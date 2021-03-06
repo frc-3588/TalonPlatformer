8ea3a31d98fd727fd0747a339df5e0546b7e4bcf
Johlesa <joh.orm000@rentonstudent.us>
2021-03-06 22:19:45 +0000

    added to player class

--- core/src/com/mygdx/game/Player.java
+++ core/src/com/mygdx/game/Player.java
@@ -1,18 +1,46 @@
 package com.mygdx.game;
 
 public class Player {
-    private int lives;
-    private String playerName;
+    private static int lives;
+    private static String playerName;
+    private static boolean playerIsDead = false;
 
     public Player(String playerName, int lives){
         this.playerName = playerName;
         this.lives = lives;
+    }
+    public static void main(String[] args){
+        //test run for logic code
+        setName("bob");
+        setLives(3);
+        if(playerIsDead){
+            System.out.println(playerName + " has died.");
+        }
+        
     }
     public int getLives(){
         return lives;
     }
     public String getName(){
         return playerName;
+    }
+    public boolean isPlayerDead(){
+        if(lives > 0){
+            playerIsDead = false;
+        }else{
+            playerIsDead = true;
+        }
+        return playerIsDead;
+    }
+    public static void setLives(int a){
+        lives = a;
+    }
+    public static void setName(String name){
+        playerName = name;
+    }
+    public static void playerLostALife(){
+        int x = lives;
+        setLives(x-1);
     }
 
     
