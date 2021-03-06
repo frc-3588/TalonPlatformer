47c73a30411ffdccee86382a34f3477a2eea7ea8
Johlesa <joh.orm000@rentonstudent.us>
2021-03-06 22:44:39 +0000

    added to player - jo

--- core/src/com/mygdx/game/Player.java
+++ core/src/com/mygdx/game/Player.java
@@ -4,7 +4,9 @@ public class Player {
     private static int lives;
     private static String playerName;
     private static boolean playerIsDead = false;
-
+    private static boolean playerCanMove = true;
+    private static boolean playerCanJump = true;
+    //constructor
     public Player(String playerName, int lives){
         this.playerName = playerName;
         this.lives = lives;
@@ -13,16 +15,25 @@ public class Player {
         //test run for logic code
         setName("bob");
         setLives(3);
-        if(playerIsDead){
-            System.out.println(playerName + " has died.");
+        while(!playerIsDead){
+            //mvmt controls
         }
+        playerCanJump = false;
+        playerCanMove = false;
+        System.out.println(playerName + " has died.");
         
     }
+
+    ///PLAYER LIVES/////////
     public int getLives(){
         return lives;
     }
-    public String getName(){
-        return playerName;
+    public static void setLives(int a){
+        lives = a;
+    }
+    public static void playerLostALife(){
+        int x = lives;
+        setLives(x-1);
     }
     public boolean isPlayerDead(){
         if(lives > 0){
@@ -32,16 +43,22 @@ public class Player {
         }
         return playerIsDead;
     }
-    public static void setLives(int a){
-        lives = a;
+
+    /////PLAYER NAME METHODS////////
+    public String getName(){
+        return playerName;
     }
     public static void setName(String name){
         playerName = name;
     }
-    public static void playerLostALife(){
-        int x = lives;
-        setLives(x-1);
-    }
 
+    /////PLAYER MOVEMENT CTRLS/////
+
+    public void moveRight(){
+
+    }
+    public void moveLeft(){
+        
+    }
     
 }
