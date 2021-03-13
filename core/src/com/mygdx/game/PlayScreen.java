7bc1b21c67161f02e1f796896430fa60323ba12b
HoffmanDestin1 <hoffmandestin141@gmail.com>
2021-03-13 22:36:33 +0000

    Added some necessary assets and code for the PlayScreen class

--- core/src/com/mygdx/game/PlayScreen.java
+++ core/src/com/mygdx/game/PlayScreen.java
@@ -10,6 +10,8 @@ import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
+import javafx.scene.shape.Rectangle;
+
 public class PlayScreen implements Screen {
     private TalonPlatformer game;
     // Texture texture;
@@ -17,9 +19,14 @@ public class PlayScreen implements Screen {
     private Viewport gamePort;
     private Hud hud;
     
+    // Tiled map variables
     private TmxMapLoader maploader;
     private TiledMap map;
     private OrthogonalTiledMapRenderer renderer;
+
+    // Box2d variables
+    private World world;
+    private Box2DDebugRenderer b2dr;
 
     public PlayScreen(TalonPlatformer game) {
         this.game = game;
@@ -35,10 +42,40 @@ public class PlayScreen implements Screen {
          // Create our game HUD for scores/timers/level info
          hud = new Hud(game.batch);
 
+         // Load our map and setup our map renderer
          mapLoader = new TmxMapLoader();
          map = maploader.load("Talon platformer map.tmx");
          renderer = new OrthogonalTiledMapRenderer(map);
+
+         // Initially set our gamecam to be centered corretly at the start of the game
          gamecam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
+
+         // Temporary gravity
+         world = new World(new Vector2(0, 0), true);
+         b2dr = new Box2DDebugRenderer();
+
+         // Bodies and fixtures
+         BodyDef bdef = new BodyDef();
+         // Temporary
+         PolygonShape shape = new PolygonShape();
+         FixtureDef fdef = new FixtureDef();
+         Body body;
+
+         // Creating a body and fixture for every corresponding object in the each of the map layers
+
+         // Create ground bodies/fixtures
+         for (MapObject object : map.getLayers().get(2).getObjects().getByType(RectangleMapObject.class)) {
+             Rectangle rect = ((RectangleMapObject) object).getRectangle();
+
+             bdef.type = BodyDef.BodyType.StaticBody;
+             bdef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight() / 2);
+
+             body = world.createBody(bdef);
+
+             shape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
+             fdef.shape = shape;
+             body.createFixture(fdef);
+         }
     }
     
     @Override
@@ -78,7 +115,11 @@ public class PlayScreen implements Screen {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        // Render our game map
         renderer.render();
+
+        // Render our Box2DDebugLines
+        b2dr.render(world, gamecam.combined);
 
         // Set our batch to now draw what the Hud camera sees
         game.batch.setProjectionMatrix(gamecam.combined);
