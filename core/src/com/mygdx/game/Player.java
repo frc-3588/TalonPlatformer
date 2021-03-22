0619295851032aa05f7097335c952b90b9c28998
Johlesa <joh.orm000@rentonstudent.us>
2021-03-22 08:44:14 +0000

    cleaned up Player,java - jo

--- core/src/com/mygdx/game/Player.java
+++ core/src/com/mygdx/game/Player.java
@@ -1,64 +1,47 @@
 package com.mygdx.game;
 
 public class Player {
-    private static int lives;
-    private static String playerName;
-    private static boolean playerIsDead = false;
-    private static boolean playerCanMove = true;
-    private static boolean playerCanJump = true;
+    private int lives = 3;
+    private String playerName;
+    private boolean playerIsDead = false;
+    private boolean playerCanMove = true;
+    private boolean playerCanJump = true;
+
     //constructor
-    public Player(String playerName, int lives){
+    public Player(String playerName){
         this.playerName = playerName;
-        this.lives = lives;
     }
-    public static void main(String[] args){
-        //test run for logic code
-        setName("bob");
-        setLives(3);
-        while(!playerIsDead){
-            //mvmt controls
-        }
-        playerCanJump = false;
-        playerCanMove = false;
-        System.out.println(playerName + " has died.");
-        
+
+    public void resetPlayer(){
+        lives = 3;
+        playerIsDead = false;
+        playerCanMove = true;
+        playerCanJump = true;
     }
 
     ///PLAYER LIVES/////////
-    public int getLives(){
-        return lives;
+    public void setLives(int x){
+        lives = x;
     }
-    public static void setLives(int a){
-        lives = a;
-    }
-    public static void playerLostALife(){
+    public void lostALife(){
         int x = lives;
         setLives(x-1);
     }
     public boolean isPlayerDead(){
-        if(lives > 0){
-            playerIsDead = false;
-        }else{
+        if(lives == 0){
             playerIsDead = true;
+            playerCanJump = false;
+            playerCanMove = false;
         }
         return playerIsDead;
     }
 
-    /////PLAYER NAME METHODS////////
+    /////PLAYER NAME////////
     public String getName(){
         return playerName;
     }
-    public static void setName(String name){
+    public void setName(String name){
         playerName = name;
-    }
-
-    /////PLAYER MOVEMENT CTRLS/////
-
-    public void moveRight(){
-
-    }
-    public void moveLeft(){
-        
     }
     
 }
