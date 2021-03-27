26965a018ea6bce5ecf5abc1a21a1c5afc6c8090
Johlesa <joh.orm000@rentonstudent.us>
2021-03-27 20:23:20 +0000

    + destin's code from Player

--- core/src/com/mygdx/game/Player.java
+++ core/src/com/mygdx/game/Player.java
@@ -1,14 +1,24 @@
 package com.mygdx.game;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.World;
 
-public class Player {
+public class Player extends Sprite{
     private int lives = 3;
     private String playerName;
     private boolean playerIsDead = false;
     private boolean playerCanMove = true;
     private boolean playerCanJump = true;
+    public World world;
+    public Body b2body;
 
     //constructor
-    public Player(String playerName){
+    public Player(World world, String playerName) {
+        this.world = world;
+        definePlayer();
         this.playerName = playerName;
     }
 
@@ -17,6 +27,20 @@ public class Player {
         playerIsDead = false;
         playerCanMove = true;
         playerCanJump = true;
+    }
+
+    public void definePlayer() {
+        BodyDef bdef = new BodyDef();
+        bdef.position.set(32 / TalonPlatformer.PPM, 32 / TalonPlatformer.PPM);
+        bdef.type = BodyDef.BodyType.DynamicBody;
+        b2body = world.createBody(bdef);
+
+        FixtureDef fdef = new FixtureDef();
+        CircleShape shape = new CircleShape();
+        shape.setRadius(5 / TalonPlatformer.PPM);
+
+        fdef.shape = shape;
+        b2body.createFixture(fdef);
     }
 
     ///PLAYER LIVES/////////
@@ -43,5 +67,13 @@ public class Player {
     public void setName(String name){
         playerName = name;
     }
+
+    /*
+    
+
+    
+
+    
+    */
     
 }
