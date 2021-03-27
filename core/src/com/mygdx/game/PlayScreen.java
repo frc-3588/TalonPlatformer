bb33722c4640ca4762d0438ccd5d60b922102108
Johlesa <joh.orm000@rentonstudent.us>
2021-03-27 20:52:17 +0000

    +destin's PlayScreen code

--- core/src/com/mygdx/game/PlayScreen.java
+++ core/src/com/mygdx/game/PlayScreen.java
@@ -1,9 +1,83 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
-public class PlayScreen implements Screen{
+import javafx.scene.shape.Rectangle;
 
+public class PlayScreen implements Screen {
+    private TalonPlatformer game;
+    // Texture texture;
+    private OrthographicCamera gamecam;
+    private Viewport gamePort;
+    private Hud hud;
+    
+    // Tiled map variables
+    private TmxMapLoader maploader;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
+
+    // Box2d variables
+    private World world;
+    private Box2DDebugRenderer b2dr;
+
+    // Player sprite
+    private Player player;
+
+    public PlayScreen(TalonPlatformer game) {
+        this.game = game;
+         // Subject to change
+         // texture = new Texture("badlogic.jpg");
+
+         // Create a camera that is used to follow the player through the TalonPlatformer world
+         gamecam = new OrthographicCamera();
+
+         // Create a FitViewPort to maintain virtual aspect ratio despite what the player does with the screen size
+         gamePort = new FitViewport(TalonPlatformer.V_WIDTH / TalonPlatformer.PPM, TalonPlatformer.V_HEIGHT / TalonPlatformer.PPM, gamecam);
+
+         // Create our game HUD for scores/timers/level info
+         hud = new Hud(game.batch);
+
+         // Load our map and setup our map renderer
+         maploader = new TmxMapLoader();
+         map = maploader.load("Talon platformer map.tmx");
+         renderer = new OrthogonalTiledMapRenderer(map, 1 / TalonPlatformer.PPM);
+
+         // Initially set our gamecam to be centered corretly at the start of the game
+         gamecam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
+
+         // World creation with gravity
+         world = new World(new Vector2(0, -10), true);
+
+         // Allows for debug lines of our box2d world
+         b2dr = new Box2DDebugRenderer();
+
+         new B2WorldCreator(world, map);
+
+         // Create the player in our game world
+         player = new Player(world);
+    }
+    
     @Override
     public void show()
     {
@@ -11,18 +85,62 @@ public class PlayScreen implements Screen{
 
     }
 
+    public void handleInput(float deltaTime) {
+        if (Gdx.input.isKeyJustPressed(Input.Keys.UP)) {
+            player.b2body.applyLinearImpulse(new Vector2(0, 4f), player.b2body.getWorldCenter(), true);
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) && player.b2body.getLinearVelocity().x <= 2) {
+            player.b2body.applyLinearImpulse(new Vector2(0.1f, 0), player.b2body.getWorldCenter(), true);
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT) && player.b2body.getLinearVelocity().x >= -2) {
+            player.b2body.applyLinearImpulse(new Vector2(-0.1f, 0), player.b2body.getWorldCenter(), true);
+        }
+    }
+
+    public void update(float deltaTime) {
+        handleInput(deltaTime);
+
+        world.step(1/60f, 6, 2);
+
+        gamecam.position.x = player.b2body.getPosition().x;
+
+        gamecam.update();
+        renderer.setView(gamecam);
+    }
+
     @Override
     public void render(float delta) 
-    {
-        
+    {   
+        // Seperate our update logic from render
+        update(delta);
 
+        // Clear the game screen with Black
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        // Render our game map
+        renderer.render();
+
+        // Render our Box2DDebugLines
+        b2dr.render(world, gamecam.combined);
+
+        // Set our batch to now draw what the Hud camera sees
+        game.batch.setProjectionMatrix(gamecam.combined);
+        game.batch.begin();
+
+        // game.batch.draw(texture, 0, 0);
+        game.batch.end();
+
+        game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
+        hud.stage.draw();
     }
 
     @Override
     public void resize(int width, int height)
     {
-        
-
+        gamePort.update(width, height);
     }
 
     @Override
@@ -49,8 +167,11 @@ public class PlayScreen implements Screen{
     @Override
     public void dispose() 
     {
-    
-
+        map.dispose();
+        renderer.dispose();
+        world.dispose();
+        b2dr.dispose();
+        hud.dispose();
     }
     
 }
