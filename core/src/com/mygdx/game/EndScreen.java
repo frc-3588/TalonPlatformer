32ca98a8a948b28b9024ff8c8daa2ccd5458ad75
Johlesa <joh.orm000@rentonstudent.us>
2021-04-24 20:42:20 +0000

    updated endscreen -jo

--- core/src/com/mygdx/game/EndScreen.java
+++ core/src/com/mygdx/game/EndScreen.java
@@ -13,9 +13,8 @@ import com.badlogic.gdx.math.Vector2;
 
 /* Things to be done:
 End Screen
-- Either load a win or lose image for the background depending on what happened in the play screen
-- Loaded image for play again button
-- Switch to the play screen when the button is clicked (both computers and touch screens)
+- Loaded image for play again button - done
+- Switch to the play screen when the button is clicked (both computers and touch screens)-done
 */
 public class EndScreen implements Screen {
     TalonPlatformer game;
@@ -25,31 +24,22 @@ public class EndScreen implements Screen {
     float width = 100;
     float height = 75;
 
+    boolean playerLost = false;
+
     public EndScreen(TalonPlatformer game) {
         this.game = game;
+
+        playerLost = Player.isPlayerDead();//state.get smthg
     }
     @Override
     public void show() 
     {
-        /*Gdx.input.setInputProcessor(new InputAdapter() {
-
-            @Override
-            public boolean keyDown(int keyCode) {
-
-                if (keyCode == Input.Keys.ENTER) {
-                    game.setScreen(new MenuScreen(game));
-                }
-
-                return true;
-            }
-        });
-        */
         //new
         Gdx.input.setInputProcessor(new InputAdapter() {
             @Override
             public boolean touchDown(int x, int y, int pointer, int button) {
                 int renderY = Gdx.graphics.getHeight() - y;
-                if (Vector2.dst(rectX, rectY, x, renderY) < 50) {//find some variable to call "50"
+                if (Vector2.dst(rectX, rectY, x, renderY) < 50) {
                     game.setScreen(new MenuScreen(game));
                 }
                 return true;
@@ -61,21 +51,31 @@ public class EndScreen implements Screen {
     @Override
     public void render(float delta) 
     {
-     Gdx.gl.glClearColor(.25f, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-       //new 
+        //4.24.2021 new code
 
-        game.shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-        game.shapeRenderer.setColor(58, 29, 0, 1); //brown is (58, 29, 0)
-        game.shapeRenderer.rect(rectX, rectY, width, height);
-        game.shapeRenderer.end();
-        //
+        Gdx.gl.glClearColor(0f, 0f, 0f, 1);
+		dx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
         game.batch.begin();
-        game.font.draw(game.batch, "You win!", Gdx.graphics.getWidth() * .25f, Gdx.graphics.getHeight() * .75f);
-        game.font.draw(game.batch, "Press this button to restart.", rectX, rectY);
-        
+        batch.draw(background, 0, 0);
         game.batch.end();
 
+        game.shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        game.shapeRenderer.setColor(58, 29, 0, 1);
+        game.shapeRenderer.rect(rectX, rectY, width, height);
+        game.shapeRenderer.end();
+
+        game.batch.begin();
+        if(playerLost){
+            game.font.draw(game.batch, "Game Over", Gdx.graphics.getWidth() * .25f, Gdx.graphics.getHeight() * .75f);
+
+        }else{
+            game.font.draw(game.batch, "You win!", Gdx.graphics.getWidth() * .25f, Gdx.graphics.getHeight() * .75f);
+
+        }
+        game.font.draw(game.batch, "Press this button to restart.", rectX, rectY);
+        game.batch.end();
+        
     }
 
     @Override
@@ -102,15 +102,13 @@ public class EndScreen implements Screen {
     @Override
     public void hide() 
     {
-        Gdx.input.setInputProcessor(null);
 
     }
 
     @Override
     public void dispose() 
     {
-    
-
+        background.dispose();
     }
     
 }
