45e84e1313dddb0a3a491b40712db5122d41f5dc
Johlesa <joh.orm000@rentonstudent.us>
2021-05-01 20:33:52 +0000

    took out button, click anywhere to restart- jo

--- core/src/com/mygdx/game/EndScreen.java
+++ core/src/com/mygdx/game/EndScreen.java
@@ -19,13 +19,6 @@ End Screen
 public class EndScreen implements Screen {
     TalonPlatformer game;
 
-    float rectX = 250;
-    float rectY = 150;
-    float width = 100;
-    float height = 75;
-
-    Texture texture = new Texture("Texture.png");
-
     public EndScreen(TalonPlatformer game) {
         this.game = game;
 
@@ -33,20 +26,22 @@ public class EndScreen implements Screen {
     }
 
     @Override
-    public void show() 
-    {
-        //new
+    public void show() {
         Gdx.input.setInputProcessor(new InputAdapter() {
+
             @Override
-            public boolean touchDown(int x, int y, int pointer, int button) {
-                int renderY = Gdx.graphics.getHeight() - y;
-                if (Vector2.dst(rectX, rectY, x, renderY) < 50) {
+            public boolean keyDown(int keyCode) {
+                //try this first. if nothing happens, try the second one that I commented out
+                if (keyCode == Input.Keys.ANY_KEY) {
                     game.setScreen(new MenuScreen(game));
                 }
+            //    if(Gdx.input.isKeyPressed(Keys.ANY_KEY)) {
+            //        game.setScreen(new MenuScreen(game));
+            //    }
+
                 return true;
             }
         });
-
     }
 
     @Override
@@ -57,23 +52,13 @@ public class EndScreen implements Screen {
         Gdx.gl.glClearColor(0f, 0f, 0f, 1);
 		dx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        //game.batch.begin();
-        //batch.draw(background, 0, 0);
-        //game.batch.end();
-
-        game.shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-        game.shapeRenderer.setColor(0, 1, 0, 0);//should be green
-        game.shapeRenderer.rect(rectX, rectY, width, height);
-        game.shapeRenderer.end();
-
         game.batch.begin();
-        game.font.draw(game.batch, "RESTART", rectX + 10, rectY + 50);//this should appear inside the box
+        game.font.draw(game.batch, "Click anywhere to RESTART", rectX + 10, rectY + 50);//this should appear inside the box
         game.batch.end();
 
         game.batch.begin();
         if(Player.getState() == State.DEAD){
             game.font.draw(game.texture, "Game Over", Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-            //game.font.draw(game.batch, "Game Over", Gdx.graphics.getWidth() * .25f, Gdx.graphics.getHeight() * .75f);
 
         }else{
             game.font.draw(game.texture, "You win!", Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
